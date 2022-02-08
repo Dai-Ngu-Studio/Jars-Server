57b0a0d78f1ebf380aeb66afad7c149a72b84865
ngianguyen81 <ngianguyen81@gmail.com>
2022-02-08 20:59:57 +0700

    configure FirebaseAdmin SDK

--- JARS/JARS-API/Program.cs
+++ JARS/JARS-API/Program.cs
@@ -1,3 +1,5 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 using JARS_DAL.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -12,6 +14,12 @@ builder.Services.AddScoped<IBillRepository, BillRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+FirebaseApp.Create(new AppOptions()
+{
+    Credential = GoogleCredential.GetApplicationDefault(),
+});
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.Authority = "https://securetoken.google.com/jars-c19f8";
