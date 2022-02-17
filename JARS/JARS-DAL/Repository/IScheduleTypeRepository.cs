8a75b57188bba838ef28c1805ebfdf6927fd49fb
ngianguyen81 <ngianguyen81@gmail.com>
2022-02-17 21:59:51 +0700

    add ScheduleType DAO,Repository

---
+++ JARS/JARS-DAL/Repository/IScheduleTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using JARS_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JARS_DAL.Repository
+{
+    public interface IScheduleTypeRepository
+    {
+        public abstract Task<IEnumerable<ScheduleType>> GetListAsync();
+        public abstract Task<ScheduleType?> GetAsync(int id);
+        public abstract Task AddAsync(ScheduleType scheduleType);
+        public abstract Task UpdateAsync(ScheduleType scheduleType);
+        public abstract Task DeleteAsync(ScheduleType scheduleType);
+    }
+}
