90437b5ab418979171843f3c523ca3132cbee055
HT-Dat <hotiendat661@gmail.com>
2022-03-14 11:45:35 +0700

    Add send noti, create bill to service project

--- JARS/JARS_Services/Worker.cs
+++ JARS/JARS_Services/Worker.cs
@@ -1,20 +1,63 @@
+using FirebaseAdmin.Messaging;
+using JARS_API.Utilities;
+using JARS_DAL.Repository;
+
 namespace JARS_Services;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+    private readonly IContractRepository _contractRepository = new ContractRepository();
 
+    private readonly ILogger<Worker> _logger;
+    private Timer _timer = null;
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
 
+
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
-        }
+        _logger.LogInformation("Timed Hosted Service running.");
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero,
+            TimeSpan.FromDays(1));
     }
+    private void DoWork(object? state)
+    {
+        var contracts = _contractRepository.CreateBillByContract();
+        contracts.Wait();
+        List<Message> messages = new List<Message>();
+        foreach (var contract in contracts.Result)
+        {
+            foreach (var accountDevice in contract.Account.AccountDevices)
+            {
+                Notification noti = new NotificationBuilder().AddTitle("JARS")
+                    .AddBody("A bill was created for contract " + contract.Name).Build();
+                Message messageForA = new MessageBuilder().AddToken(accountDevice.FcmToken).AddNotification(noti)
+                    .Build();
+                messages.Add(messageForA);
+            }
+        }
+
+        try
+        {
+            BatchResponse batchResponse = FirebaseCloudMessagingUtility.SendMessagesAsync(messages)
+                .GetAwaiter().GetResult();
+            FcmTokenHandler.HandleBatchResponse(batchResponse, messages).GetAwaiter();
+        }
+        catch (FirebaseMessagingException)
+        {
+//burh
+        }
+        catch (Exception)
+        {
+            // log
+        }
+
+        _logger.LogInformation(
+            "Timed Hosted Service is working");
+    }
+
 }
