6dfcd0a19f1830668d203305ae995f2158d505d4
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-21 01:16:17 +0700

    add model for report

---
+++ JARS/JARS-API/BusinessModels/AnalyticsReport.cs
@@ -0,0 +1,14 @@
+﻿namespace JARS_API.BusinessModels
+{
+    public class AnalyticsReport
+    {
+        public AnalyticsReport(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
