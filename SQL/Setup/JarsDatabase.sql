fe0c83601e678ec089e1695d7416d63e01595f9e
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-20 17:42:52 +0700

    fix typo; remove identity for scheduleType

--- SQL/Setup/JarsDatabase.sql
+++ SQL/Setup/JarsDatabase.sql
@@ -105,7 +105,7 @@ IF OBJECT_ID('ScheduleType', 'U') IS NULL
 BEGIN
   CREATE TABLE [ScheduleType]
   (
-    [ID] int IDENTITY(1, 1),
+    [ID] int,
     --PK--
     [Name] nvarchar(max),
 
@@ -229,6 +229,6 @@ BEGIN
 
     PRIMARY KEY ([FcmToken]),
 
-    FOREIGN KEY ([AccountId]) REFERENCES Account([ID]),
+    FOREIGN KEY ([AccountID]) REFERENCES Account([ID]),
   );
 END;
