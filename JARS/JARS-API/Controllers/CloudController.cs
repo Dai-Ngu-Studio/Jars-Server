381ccb6e0ad1bd04b1ce4e311b76df13fdb1d1e9
ngianguyen81 <ngianguyen81@gmail.com>
2022-03-21 01:16:34 +0700

    format data from analytics

--- JARS/JARS-API/Controllers/CloudController.cs
+++ JARS/JARS-API/Controllers/CloudController.cs
@@ -1,6 +1,7 @@
 ﻿using FirebaseAdmin.Auth;
 using Google.Analytics.Data.V1Beta;
 using Google.Cloud.Storage.V1;
+using JARS_API.BusinessModels;
 using JARS_DAL.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -112,12 +113,12 @@ namespace JARS_API.Controllers
             string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
 
             List<string> dateRanges = new List<string>();
-            for (int i = -7; i < 0; i++)
+            for (int i = -13; i < 0; i++)
             {
                 dateRanges.Add(DateTime.Now.AddDays(i).ToString("yyyy-MM-dd"));
             }
 
-            RunReportRequest request = new RunReportRequest
+            RunReportRequest farRequest = new RunReportRequest
             {
                 Property = "properties/" + 306844081,
                 Dimensions = { new Dimension { Name = "city" }, },
@@ -127,17 +128,87 @@ namespace JARS_API.Controllers
                     new Metric { Name = "totalUsers" },
                 },
                 DateRanges = {
-                    new DateRange { StartDate = dateRanges[0], EndDate = dateRanges[1], Name = "7 to 6 days ago" },
-                    new DateRange { StartDate = dateRanges[2], EndDate = dateRanges[3], Name = "5 to 4 days ago" },
-                    new DateRange { StartDate = dateRanges[4], EndDate = dateRanges[5], Name = "3 to 2 days ago" },
-                    new DateRange { StartDate = dateRanges[6], EndDate = currentDate, Name = "yesterday and today" },
+                    new DateRange { StartDate = dateRanges[0], EndDate = dateRanges[1], Name = "13 to 12 days ago" },
+                    new DateRange { StartDate = dateRanges[2], EndDate = dateRanges[3], Name = "11 to 10 days ago" },
+                    new DateRange { StartDate = dateRanges[4], EndDate = dateRanges[5], Name = "9 to 8 days ago" },
+                    new DateRange { StartDate = dateRanges[6], EndDate = dateRanges[7], Name = "7 to 6 days ago" },
                 },
             };
 
-            var response = await client.RunReportAsync(request);
+            RunReportRequest closeRequest = new RunReportRequest
+            {
+                Property = "properties/" + 306844081,
+                Dimensions = { new Dimension { Name = "city" }, },
+                Metrics = {
+                    new Metric { Name = "active28DayUsers" },
+                    new Metric { Name = "newUsers" },
+                    new Metric { Name = "totalUsers" },
+                },
+                DateRanges = {
+                    new DateRange { StartDate = dateRanges[8], EndDate = dateRanges[9], Name = "5 to 4 days ago" },
+                    new DateRange { StartDate = dateRanges[10], EndDate = dateRanges[11], Name = "3 to 2 days ago" },
+                    new DateRange { StartDate = dateRanges[12], EndDate = currentDate, Name = "yesterday and today" },
+                },
+            };
+
+            BatchRunReportsRequest requests = new BatchRunReportsRequest();
+            requests.Requests.Add(farRequest);
+            requests.Requests.Add(closeRequest);
+            requests.Property = "properties/" + 306844081;
+
+            var response = await client.BatchRunReportsAsync(requests);
+
+            List<AnalyticsReport> farActive28Reports = new();
+            List<AnalyticsReport> closeActive28Reports = new();
+
+            List<AnalyticsReport> farNewReports = new();
+            List<AnalyticsReport> closeNewReports = new();
+
+            List<AnalyticsReport> farTotalReports = new();
+            List<AnalyticsReport> closeTotalReports = new();
+
+
+            foreach (var row in response.Reports[0].Rows)
+            {
+                if (row.DimensionValues[0].Value == "Ho Chi Minh City")
+                {
+                    farActive28Reports.Add(new AnalyticsReport(row.DimensionValues[1].Value, row.MetricValues[0].Value));
+                    farNewReports.Add(new AnalyticsReport(row.DimensionValues[1].Value, row.MetricValues[1].Value));
+                    farTotalReports.Add(new AnalyticsReport(row.DimensionValues[1].Value, row.MetricValues[2].Value));
+                }
+            }
+
+            foreach (var row in response.Reports[1].Rows)
+            {
+                if (row.DimensionValues[0].Value == "Ho Chi Minh City")
+                {
+                    closeActive28Reports.Add(new AnalyticsReport(row.DimensionValues[1].Value, row.MetricValues[0].Value));
+                    closeNewReports.Add(new AnalyticsReport(row.DimensionValues[1].Value, row.MetricValues[1].Value));
+                    closeTotalReports.Add(new AnalyticsReport(row.DimensionValues[1].Value, row.MetricValues[2].Value));
+                }
+            }
+
+            Dictionary<string, object> reports = new();
+
+            farActive28Reports.Reverse();
+            closeActive28Reports.Reverse();
+            farActive28Reports.AddRange(closeActive28Reports);
+
+            farNewReports.Reverse();
+            closeNewReports.Reverse();
+            farNewReports.AddRange(closeNewReports);
+
+            farTotalReports.Reverse();
+            closeTotalReports.Reverse();
+            farTotalReports.AddRange(closeTotalReports);
+
+            reports.Add("active28DayUsers", farActive28Reports);
+            reports.Add("newUsers", farNewReports);
+            reports.Add("totalUsers", farTotalReports);
+
             Dictionary<string, object> data = new Dictionary<string, object>();
 
-            data.Add("report", response);
+            data.Add("reports", reports);
             data.Add("todayTransactions", (await _transactionRepository.GetTransactionsFromDate(DateTime.Today)).Count());
             return Ok(data);
         }
