6e4855ff87c07b70ad61a38686f68da1839f5c2a
HT-Dat <hotiendat661@gmail.com>
2022-03-25 02:32:39 +0700

    Add new 1 minute timer for the DemoWorker

--- JARS/JARS_Services/Worker.cs
+++ JARS/JARS_Services/Worker.cs
@@ -10,11 +10,12 @@ public class Worker : BackgroundService
 
     private readonly ILogger<Worker> _logger;
     private Timer _timer = null;
+    private Timer _timerDemo = null;
+
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
-
 
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -23,7 +24,45 @@ public class Worker : BackgroundService
 
         _timer = new Timer(DoWork, null, TimeSpan.Zero,
             TimeSpan.FromDays(1));
+        _timerDemo = new Timer(DoWorkDemo, null, TimeSpan.Zero,
+            TimeSpan.FromMinutes(1));
     }
+
+    private void DoWorkDemo(object? state)
+    {
+        var contracts = _contractRepository.CreateBillByContractDemo();
+        contracts.Wait();
+        List<Message> messages = new List<Message>();
+        foreach (var contract in contracts.Result)
+        {
+            foreach (var accountDevice in contract.Account.AccountDevices)
+            {
+                Notification noti = new NotificationBuilder().AddTitle("JARS")
+                    .AddBody("A bill was created for contract " + contract.Name).Build();
+                Message messageForA = new MessageBuilder().AddToken(accountDevice.FcmToken).AddNotification(noti)
+                    .Build();
+                messages.Add(messageForA);
+            }
+        }
+
+        try
+        {
+            BatchResponse batchResponse = FirebaseCloudMessagingUtility.SendMessagesAsync(messages)
+                .GetAwaiter().GetResult();
+            FcmTokenHandler.HandleBatchResponse(batchResponse, messages).GetAwaiter();
+        }
+        catch (FirebaseMessagingException)
+        {
+        }
+        catch (Exception)
+        {
+            // log
+        }
+
+        _logger.LogInformation(
+            "Demo background service is running");
+    }
+
     private void DoWork(object? state)
     {
         var contracts = _contractRepository.CreateBillByContract();
@@ -59,5 +98,4 @@ public class Worker : BackgroundService
         _logger.LogInformation(
             "Timed Hosted Service is working");
     }
-
 }
